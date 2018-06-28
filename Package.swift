a3fbd2f2ff9c290cc5a33d807e3d8a4514982c38
xlsn0w <xlsn0wios@gmail.com>
2018-06-28 18:21:45 +0800

    add

--- Package.swift
+++ Package.swift
@@ -3,28 +3,13 @@
 
 import PackageDescription
 
+let urls = ["https://github.com/PerfectlySoft/Perfect-HTTPServer.git"]
+let versions = Version(0,0,0)..<Version(10,0,0)
+
 let package = Package(
-    name: "SwiftServer",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftServer",
-            targets: ["SwiftServer"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftServer",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftServerTests",
-            dependencies: ["SwiftServer"]),
-    ]
+    name: "Perfect",
+    targets: [],
+    dependencies: urls.map{.Package(url: $0, versions: versions)}
 )
 
 /*
